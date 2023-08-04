e2de628f931304b27eaf3bf5d8702a68bc52d795
Ali <alirezamonfared813@gmail.com>
2023-08-04 09:03:27 -0700

    l

--- Leet ocde(1)/main.swift
+++ Leet ocde(1)/main.swift
@@ -2,10 +2,36 @@
 //  main.swift
 //  Leet ocde(1)
 //
-//  Created by Alireza Karimi on 2023-08-04.
+//  Created by Alireza Karimi on 2023-08-03.
 //
 
 import Foundation
 
-print("Hello, World!")
+class TreeNode {
+    var val: Int
+    var left: TreeNode?
+    var right: TreeNode?
+
+    init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+    if root == nil || root === p || root === q {
+        return root
+    }
+
+    let leftLCA = lowestCommonAncestor(root?.left, p, q)
+    let rightLCA = lowestCommonAncestor(root?.right, p, q)
+
+    if leftLCA != nil && rightLCA != nil {
+        return root
+    }
+
+    return leftLCA ?? rightLCA
+}
+
 
